b8439365e95f3d1d423d67d93930d7c17f898ecb
Caitano <josiasmartinscaitano@gmail.com>
2022-11-01 18:23:25 -0300

    feat: separando responsabilidades

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -2,6 +2,10 @@ import { handlerStatus } from '../utils/promise-helpers.js';
 
 const API = 'http://localhost:3000/notas';
 
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+
 const sumItems = code => notas => notas
     .$flatMap(nota => nota.itens)
     .filter(item => item.codigo == code)
