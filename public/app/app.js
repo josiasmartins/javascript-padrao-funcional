fc7e8cb2c6f220c227de3dc97baf100beeb074d3
Caitano <josiasmartinscaitano@gmail.com>
2022-10-31 21:54:50 -0300

    feat: somando todos os itens com codigo especifico

--- public/app/app.js
+++ public/app/app.js
@@ -6,5 +6,13 @@ document
         // fecth: api promisificada
         fetch('http://localhost:3000/notas')
         .then(handlerStatus)
+        .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+        .then(itens => {
+            console.log(itens);
+            return itens;
+        })
+        .then(itens => itens.filter(item => item.codigo == '2143'))
+        .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+        .then(console.log)
         .then(notas => console.log(notas))
         .catch(console.log);
