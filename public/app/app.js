2ce54fe69d7bdd982bc3d6844de996986facc28d
Caitano <josiasmartinscaitano@gmail.com>
2022-11-01 18:15:54 -0300

    feat: a classe de servico

--- public/app/app.js
+++ public/app/app.js
@@ -1,18 +1,15 @@
 import { handlerStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js'
-
-const sumItems = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
+import { notasService as service } from './nota/service.js';
 
 document
     .querySelector('#myButton')
     .onclick = () => 
-        // fecth: api promisificada
-        fetch('http://localhost:3000/notas')
-        .then(handlerStatus)
-        // .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
-        .then(sumItems('2143'))
-        .then(console.log)
-        .catch(console.log);
+        service
+            // fecth: api promisificada
+            // fetch('http://localhost:3000/notas')
+            // .then(handlerStatus)
+            // .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+            .sumItems('2143')
+            .then(console.log)
+            .catch(console.log);
---
+++ public/app/nota/service.js
@@ -0,0 +1,20 @@
+import { handlerStatus } from '../utils/promise-helpers.js';
+
+const API = 'http://localhost:3000/notas';
+
+const sumItems = code => notas => notas
+    .$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
+
+export const notasService = {
+
+    listAll() {
+        
+        return fetch(API).then(handlerStatus)
+    },
+
+    sumItems(code) {
+        return this.listAll().then(sumItems(code));
+    }
+}
