12c9acacac3d473bdc5ac0517470edfbec40538a
Caitano <josiasmartinscaitano@gmail.com>
2022-11-01 18:04:46 -0300

    feat: deixando clara nossa intençao

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,11 @@
 import { handlerStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js'
 
+const sumItems = code => notas => notas
+    .$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
+
 document
     .querySelector('#myButton')
     .onclick = () => 
@@ -8,10 +13,6 @@ document
         fetch('http://localhost:3000/notas')
         .then(handlerStatus)
         // .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
-        .then(notas => notas
-            .$flatMap(nota => nota.itens)
-            .filter(item => item.codigo == '2143')
-            .reduce((total, item) => total + item.valor, 0)
-        )
+        .then(sumItems('2143'))
         .then(console.log)
         .catch(console.log);
