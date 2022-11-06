be6ed1d95a39693c53245186ee28297fc78995b5
Caitano <josiasmartinscaitano@gmail.com>
2022-11-06 13:05:27 -0300

    feat: desaclopando o codigo

--- public/app/utils/event-emitter.js
+++ public/app/utils/event-emitter.js
@@ -3,12 +3,14 @@ const events = new Map();
 export const EventEmitter = {
 
     on(event, listener) {
+        // set: Adicionamos novas chaves/valores através do método set.
         if (!events.has(event)) events.set(event, []);
+        // get: através do método get obtermos o dado associado à chave passada como parâmetro para o método.
         events.get(event).push(listener);
     },
 
     emit(event, data) {
-
+        // get: através do método get obtermos o dado associado à chave passada como parâmetro para o método.
         const listeners = events.get(event);
         if (listeners) 
             listeners.forEach(listener => listener(data));
